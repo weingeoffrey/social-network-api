25d4f97bb50490e457b7571b28029c89479882c6
weinge <wein.geoffrey@gmail.com>
2021-09-09 20:13:28 -0500

    User schema

--- models/User.js
+++ models/User.js
@@ -1,1 +1,34 @@
 const { Schema, model, Types } = require('mongoose');
+
+const UserSchema = new Schema(
+	{
+		username: {
+			type: String,
+			required: true,
+			trim: true,
+			unique: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+			match: [
+				/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
+				'Please enter a valid email address',
+			],
+		},
+		thoughts: [{ type: ObjectId, ref: 'Thought' }],
+		friends: [{ type: ObjectId, ref: this }],
+	},
+	{
+		toJSON: {
+			virtuals: true,
+			getters: true,
+		},
+		id: false,
+	}
+);
+
+UserSchema.virtual('friendCount').get(function () {
+	return this.friends.length;
+});
